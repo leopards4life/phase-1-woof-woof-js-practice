bafc9831664de2f634fab42049513f4fac54612f
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-02-09 14:34:13 -0700

    Add patch request for dog toggle

--- src/index.js
+++ src/index.js
@@ -8,22 +8,32 @@ function fetchDogs() {
     .then(data => renderDogs(data))
 };
 
-function dogToggle() {
+function dogToggle(id, isGoodDog) {
     let goodDogButton = document.getElementById("good-dog-button");
-    (goodDogButton.innerHTML)
+    if (goodDogButton.innerHTML === "Bad Dog!") {
+        goodDogButton.innerHTML = "Good Dog!"
+    } else goodDogButton.innerHTML = "Bad Dog!";
+    fetch(`http://localhost:3000/pups/${id}` , {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "isGoodDog": isGoodDog ? false : true
+        })
+    })
 };
-
-const toggleButton = document.getElementById("toggleButton");
-console.log(toggleButton);
-toggleButton.addEventListener("click", dogToggle)
 
 function handleDogClick(event) {
     let isGoodDog = (event.target.props);
     let dogInfo = document.getElementById("dog-info");
     let dogCard = document.createElement("div");
-    dogCard.innerHTML=`<img src=${event.target.image}>
+    dogCard.id = (event.target.id);
+    dogCard.innerHTML=`
+    <img src=${event.target.image}>
     <h2>${event.target.innerHTML}</h2>
-    <button onclick="dogToggle()" id="good-dog-button">${isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>`
+    <button onclick="dogToggle(${dogCard.id}, ${isGoodDog})" id="good-dog-button">${isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>`
     dogInfo.appendChild(dogCard);
 };
 
@@ -34,7 +44,7 @@ function renderDogs(dogs) {
        card.innerHTML=`${dog.name}`;
        card.id=`${dog.id}`; 
        card.image=`${dog.image}`;
-       card.props=`isGoodDog=${dog.isGoodDog}`;
+       card.props= dog.isGoodDog;
        card.addEventListener("click", handleDogClick);
        dogBar.appendChild(card);
     })
